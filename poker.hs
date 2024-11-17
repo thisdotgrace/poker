d89afa09e181810da1eed90c5ee3ed6bb4b70adc
Grace Bennett <gbennett2@sheffield.ac.uk>
2024-11-17 19:50:47 +0000

    Created a Player data type, and a tests.txt file to store useful test inputs

--- poker.hs
+++ poker.hs
@@ -1,24 +1,34 @@
 
+
+-- Defines sum types for suit and value 
 data Suit = Hearts | Diamonds | Spades | Clubs 
             deriving (Eq, Show)
-
 data Value = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten
              | Jack | Queen | King | Ace 
-             deriving (Eq, Show, Enum, Bounded)
-             -- enum allows to create range (ie [Two..Ace]), and values can be ordered
-             -- bounded gives minBound, maxBound
-
+             deriving (Eq, Show, Enum, Bounded) -- enum allows to create range (ie [Two..Ace]), bounded gives minBound/maxBound
 data Card = Card Value Suit 
             deriving (Eq)
-
--- show instance so cards displayed in form 'value of suit'
-instance Show Card where
+instance Show Card where -- show instance so cards displayed in form 'value of suit'
   show (Card value suit) = show value ++ " of " ++ show suit
 
 
 -- Creates deck of cards using list comprehension
 deck :: [Card]
 deck = [ Card value suit | suit <- [Hearts, Diamonds, Spades, Clubs], value <- [Two .. Ace] ]
+
+-- Player product (can be inactive if folded out)
+data Player = 
+  Inactive |
+  Active
+  { name :: String,
+    privateCards :: [Card],
+    chips :: Int,
+    isDealer :: Bool
+    -- Behaviour :: String?
+  }
+  deriving (Show)
+
+
 
 
 main :: IO()
